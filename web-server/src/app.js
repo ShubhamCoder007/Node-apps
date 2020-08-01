c1103fbd6ef5bda9fb18f62fc7ea30f2735c4286
Shubham Banerjee <shubham.banerjee@comviva.com>
2020-08-01 13:18:08 +0530

    web servers and handlebar, partials update

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,28 +1,62 @@
 const path = require('path')
 const express = require('express')
+const fs = require('fs')
+const hbs = require('hbs')
 
+//paths 
 const pubPath = path.join(__dirname, '../public/')
+const viewPath = path.join(__dirname, '../templates/views/')
+const partialsPath = path.join(__dirname, '../templates/partials/')
 
 const app = express()
 
+//adding handlebar, key value pair
+//handle bar configurations
+app.set('views', viewPath)
+app.set('view engine', 'hbs')
+hbs.registerPartials(partialsPath)
+
+//setting up directory for servicing
 app.use(express.static(pubPath))
 
-// app.get('', (req, res) => {
-//     res.send('Welcome page')
-// })
+//setting routes to serve up the hbs file in view which is a handlebar for
+//serving dynamic web content
+app.get('', (req, res) => {
+    //passing value to render to the file
+    res.render('index', {
+        title : 'Weather App',
+        name: 'Shubham Banerjee'
+    })
+})
 
-// app.get('/help', (req, res) => {
-//     res.send('help page')
-// })
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Shubham Banerjee'
+    })
+})
 
-// app.get('/about', (req, res) => {
-//     res.send('<title>About page</title>')
-// })
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page!',
+        msg: 'How may I help you?',
+        name: 'Shubham Banerjee'
+    })
+})
 
-app.get('/weather', (req, res) => {
-    res.send({
-        temperature: 33,
-        humidity: 30
+app.get('/help/*', (req, res) => {
+    res.render('error', {
+        title: 'Error this is not the correct help url',
+        msg: 'Help page could not be found',
+        name: 'Shubham Banerjee'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('error', {
+        title: 'Page you are trying to access is wrong!',
+        msg: '404 page not found',
+        name: 'Shubham Banerjee'
     })
 })
 
